1f583498c4df91700587a6787c22fa2f07a715e4
luccachagas <luccachagas@eaportal.org>
2025-05-30 21:20:35 -0300

    criacao do main.js e criacao da funcao para adicionar itens ao array de itens

---
+++ main.js
@@ -0,0 +1,14 @@
+const item = []
+
+function addItem() {
+    const itemName = document.querySelector('#item').value
+
+    const item = {
+        name: itemName,
+        checked: false
+    }
+
+    items.push(item)
+
+    document.querySelector('#item').value = ''
+}
